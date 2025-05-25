f6f13bdbde4c4b8e96a91d2e27e1919d08d5dfff
sasaanka <sasanka2629@gmail.com>
2025-05-25 18:38:31 +0530

    Welcome

---
+++ src/main/java/com/example/demo/PayRoll2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayRoll2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayRoll2Application.class, args);
+	}
+
+}
