00f2e7aacdce3f7f1af23c7e89b3da47a934a6d8
sasaanka <sasanka2629@gmail.com>
2025-05-25 18:58:45 +0530

    UC6: Lombok is used for generating getters and setters

--- src/main/java/com/example/demo/dto/EmployeeDTO.java
+++ src/main/java/com/example/demo/dto/EmployeeDTO.java
@@ -1,19 +1,13 @@
 package com.example.demo.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class EmployeeDTO {
-    private String name;
-    private long salary;
-
-    public EmployeeDTO() {}
-
-    public EmployeeDTO(String name, long salary) {
-        this.name = name;
-        this.salary = salary;
-    }
-
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-
-    public long getSalary() { return salary; }
-    public void setSalary(long salary) { this.salary = salary; }
+     String name;
+     long salary;
 }
