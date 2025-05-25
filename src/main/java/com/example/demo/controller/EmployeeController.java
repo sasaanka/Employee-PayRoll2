fe03abf8e851141ef6d50f779eadd5ce4d7daf50
sasaanka <sasanka2629@gmail.com>
2025-05-25 18:43:54 +0530

    UC2 : Demonstrating http methods

--- src/main/java/com/example/demo/controller/EmployeeController.java
+++ src/main/java/com/example/demo/controller/EmployeeController.java
@@ -4,8 +4,24 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/employee")
 public class EmployeeController {
-    @GetMapping("/dummy")
-    public String getDummy() {
-        return "Payroll App is up and running!";
+
+    @GetMapping
+    public String getEmployee() {
+        return "GET - Employee Data";
+    }
+
+    @PostMapping
+    public String createEmployee() {
+        return "POST - Create Employee";
+    }
+
+    @PutMapping
+    public String updateEmployee() {
+        return "PUT - Update Employee";
+    }
+
+    @DeleteMapping
+    public String deleteEmployee() {
+        return "DELETE - Delete Employee";
     }
 }
